8fc23635034367458da2a05c9b6cc19fd0190dbc
Kent Eudy <keudy@vizuri.com>
2022-07-01 14:46:38 -0400

    initial

---
+++ cicd/build.sh
@@ -0,0 +1,2 @@
+tkn pipeline start build-and-deploy-hello-world-app -p GIT_REPO=https://github.com/Vizuri/hello-world-php.git --use-param-defaults
+
---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+  <head>
+    <title>Hello World!</title>
+    <style>
+      h1 {
+        text-align: center;
+      }
+      
+      h2 {
+        text-align: left;
+      }
+      
+      h3 {
+        text-align: right;
+      }
+</style>
+  </head>
+
+  <body>
+    <?php echo "<h1> Hello World! </h1>"; ?>
+    
+    <?php phpinfo(); ?>
+  </body>
+
+</html>
