e6d4a2cc8f298abc524bb896eabcdd7843b858c0
Kent Eudy <keudy@vizuri.com>
2022-07-05 09:21:22 -0400

    Fix image

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,2 +1,2 @@
-tkn pipeline start build-and-deploy-hello-world-app -p GIT_REPO=https://github.com/Vizuri/hello-world-php.git --use-param-defaults
+tkn pipeline start build-and-deploy-hello-world-app -w name=workspace,volumeClaimTemplateFile=hello-world-workspace.yaml -p GIT_REPO=https://github.com/Vizuri/hello-world-php.git --use-param-defaults
 
