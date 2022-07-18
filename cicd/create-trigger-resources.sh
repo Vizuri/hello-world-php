51b0eb5dc867cff29ef3e3b7a4340b78438f9a8d
Kent Eudy <keudy@vizuri.com>
2022-07-18 13:18:26 -0400

    Update

--- cicd/create-trigger-resources.sh
+++ cicd/create-trigger-resources.sh
@@ -1,8 +1,9 @@
-oc create --save-config=true -f hello-world-trigger-binding.yaml
-oc create --save-config=true -f hello-world-trigger-template.yaml
-oc create --save-config=true -f hello-world-trigger-trigger.yaml
-oc create --save-config=true -f hello-world-trigger-event-listener.yaml
-oc create --save-config=true -f hello-world-trigger-route.yaml
+oc apply  -f hello-world-trigger-binding.yaml
+oc apply  -f hello-world-trigger-template.yaml
+oc apply  -f hello-world-trigger-trigger.yaml
+oc delete -f hello-world-trigger-event-listener.yaml
+oc create  --save-config -f hello-world-trigger-event-listener.yaml
+oc apply  -f hello-world-trigger-route.yaml
 
 
 
